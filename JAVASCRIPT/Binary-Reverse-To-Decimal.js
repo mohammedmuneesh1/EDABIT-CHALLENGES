4e863ea136c1b597b57c91a3f6469b8dd0ab951b
mohammed muneesh E K <mohammedmuneesh1@gmail.com>
2023-12-11 17:08:06 +0530

    Binary Reverse To Decimal Added

---
+++ JAVASCRIPT/Binary-Reverse-To-Decimal.js
@@ -0,0 +1,9 @@
+// reversedBinaryInteger(10) ➞ 5
+// // 10 = 1010 -> 0101 = 5
+
+//ANSWER
+
+function reversedBinaryInteger(num){
+    return parseInt(num.toString(2).split("").reverse().join("") ,2); 
+}
+console.log(reversedBinaryInteger(10))
