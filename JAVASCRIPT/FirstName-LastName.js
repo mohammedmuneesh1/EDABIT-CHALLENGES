2fb481f96b906332eeae633dce958659e76417b9
mohammed muneesh E K <mohammedmuneesh1@gmail.com>
2024-01-10 22:55:04 +0530

    file changes made

--- JAVASCRIPT/FirstName-LastName.js
+++ JAVASCRIPT/FirstName-LastName.js
@@ -2,17 +2,20 @@
 
 const namefn = (str) => {
     const [firstName, ...lastNameArray] = str.trim().split(" ");
-    const lastName = lastNameArray.join(" ").trim();
+    console.log(lastNameArray)
+    const lastName = lastNameArray?.join(" ").trim();
+      
+   
     return {
         firstName,
-       ... (lastName !== "" &&{lastName:lastName})
+       ...(lastName && { lastName }),
     }
 }
 const data = namefn("sharif razak ek")
-console.log("first")
+// console.log("first")
 console.log(data)
 console.log(namefn("haris")) //{firstName : haris}
-console.log(namefn('haris sharif ek'))  //{firstName:haris , lastName: sharif ek }
+// console.log(namefn('haris sharif ek'))  //{firstName:haris , lastName: sharif ek }
 
 
 
--- JAVASCRIPT/trail.js
+++ JAVASCRIPT/trail.js
@@ -1,2 +1,16 @@
-const date = new Date();
-console.log(date)
+const arr = [
+    {
+      isDeleted: false,
+      _id: new ObjectId('6598d695912dd5ecdcacd2b6'),
+      optionTitle: 'coming',
+      pollQuestionId: new ObjectId('6598d695912dd5ecdcacd2b3')
+    },
+    {
+      isDeleted: false,
+      _id: new ObjectId('6598d695912dd5ecdcacd2b7'),
+      optionTitle: "interested,ican't",
+      pollQuestionId: new ObjectId('6598d695912dd5ecdcacd2b3')
+    }
+  ]
+
+console.log("first")
